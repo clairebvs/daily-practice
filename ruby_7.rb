b28cf61b5ff87d4ea4341c532f4b505bd44044dd
ClaireBvs <clairembeauvais@gmail.com>
2019-02-03 20:55:13 -0700

    Add #remove_url_anchor kata - string manipulation

--- ruby_7.rb
+++ ruby_7.rb
@@ -1,4 +1,4 @@
-# kata
+# kata codewars
 def century(year)
   if year < 100
     1
@@ -12,3 +12,9 @@ end
 year = 1705
 century(year)
 # return 18
+
+def remove_url_anchor(url)
+  url.gsub(/#.*/, '')
+end
+remove_url_anchor('www.codewars.com#about')
+# return 'www.codewars.com'
